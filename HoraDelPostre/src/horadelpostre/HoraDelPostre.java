05f44930b40f25fea249ad9597c464d7acf3fc17
Mariel Mata <marielelemc@outlook.com>
2021-12-20 15:33:14 -0600

    Vista inicio creada
    Primera vista creada, solo la de inicio

---
+++ HoraDelPostre/src/horadelpostre/HoraDelPostre.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horadelpostre;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author Mariel Mata <mlmtcrdn@gmail.com>
+ */
+public class HoraDelPostre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
